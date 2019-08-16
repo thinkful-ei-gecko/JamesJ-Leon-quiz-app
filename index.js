9ead6bee024fd050602b153e431fcd793ed66dac
Leon Dail <leondailofficial@gmail.com>
2019-08-16 11:51:14 -0700

    fixed main problem

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
  * Function loads the quiz page
  */
 function loadApp() {
-  $('section').empty();
-  $('section').append(`
+  $('main').empty();
+  $('main').append(`
     <h1>Game of Thrones Quiz</h1>
     <p>Behold - this is the toughest quiz about Games of Thrones ever devised. Enjoy!</p>
     <form id="startQuiz">
