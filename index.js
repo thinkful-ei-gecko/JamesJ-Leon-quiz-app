7a22102ebdfdf2db8034f154510e1225cb17021c
Leon Dail <leondailofficial@gmail.com>
2019-08-16 14:21:56 -0700

    progress quiz

--- index.js
+++ index.js
@@ -14,59 +14,93 @@ function loadApp() {
   `);
 }
 
-function startQuiz() {
+function playQuiz() {
   $('#startQuiz').submit( e => {
     e.preventDefault();
-    let questionIndex = database.store[database.currentQuestionNumber] - 1;
+    let questionIndex = database.store[database.currentQuestionNumber - 1];
     $('main').empty();
     $('main').append(`
     <h1>Game of Thrones Quiz</h1>
     <ul>
-      <li>Score: ${database.score}</li>
-      <li>Question: ${database.currentQuestionNumber} out of ${database.store.length} </li>
+      <li id="scoreCount">Score: ${database.score}</li>
+      <li id="questionCount">Question: ${database.currentQuestionNumber} out of ${database.store.length} </li>
     </ul>
     <form id="quizQuestions">
-      <h2>${questionIndex['question']}</h2>
-      <label for="key1">${database.store[database.currentQuestionNumber]}</label>
-      <input type="radio" name="key" id="key1" required="required" value="${database.store[database.currentQuestionNumber]['answer'][0]}" />
-      <label for="key2">${database.store[database.currentQuestionNumber]['answer'][1]}</label>
-      <input type="radio" name="key" id="key2" required="required" value="${database.store[database.currentQuestionNumber]['answer'][1]}" />
-      <label for="key3">${database.store[database.currentQuestionNumber]['answer'][2]}</label>
-      <input type="radio" name="key" id ="key3" required="required" value="${database.store[database.currentQuestionNumber]['answer'][2]}" />
-      <label for="key4">${database.store[database.currentQuestionNumber]['answer'][3]} </label>
-      <input type="radio" name="key" id ="key4" required="required" value="${database.store[database.currentQuestionNumber]['answer'][3]}" />
+      <h2>${questionIndex['question']}</h2>  
+      <label for="key1">${questionIndex['answer'][0]}</label>
+      <input type="radio" name="key" id="key1" required="required" value="${questionIndex['answer'][0]}" />
+      <label for="key2">${questionIndex['answer'][1]}</label>
+      <input type="radio" name="key" id="key2" required="required" value="${questionIndex['answer'][1]}" />
+      <label for="key3">${questionIndex['answer'][2]}</label>
+      <input type="radio" name="key" id ="key3" required="required" value="${questionIndex['answer'][2]}" />
+      <label for="key4">${questionIndex['answer'][3]} </label>
+      <input type="radio" name="key" id ="key4" required="required" value="${questionIndex['answer'][3]}" />
       <button type="submit">Submit Answer</button>
     </form>
     `);
   });
 }
 
-// $('input:checked').val()
-
 function submitAnswer() {
   $('main').on('submit','#quizQuestions', e => {
     let choice = $( 'input:checked' ).val();
+    let questionIndex = database.store[database.currentQuestionNumber - 1];
     let correct = '';
     let message = '';
-    console.log(choice);
+    let buttonTitle = 'Next Question'
     e.preventDefault();
     $('#quizQuestions').remove();
-    if (choice === database.store[database.currentQuestionNumber]['correctAnswer']) {
+    if (choice === questionIndex['correctAnswer']) {
       correct = 'You are correct!';
       message = 'Good work! Move on to the next question.';
+      database.score++;
     } else {
       correct = 'You are incorrect...';
-      message = `Correct answer was: ${database.store[database.currentQuestionNumber]['correctAnswer']}`;
+      message = `Correct answer was: ${questionIndex['correctAnswer']}`;
     }
-    
-    
+    if (database.currentQuestionNumber === database.store.length) {
+      buttonTitle = 'See Results';
+    }
+    $('main').append(`
+      <h2>${correct}</h2>
+      <p>${message}</p>
+      <form id="nextQuestion">
+      <button type="submit">${buttonTitle}</button>
+      </form>
+    `);
+    $('#scoreCount').text(`Score: ${database.score}`);
   });
 }
 
+/**
+ * Function checks to see if we're on the last question
+ * If not, progress to next question --> playQuiz();
+ * Else, displayScore();
+ */
+function progressQuiz() { 
+  if (database.currentQuestionNumber === database.store.length) {
+    displayScore();
+  }
+  else {
+    playQuiz();
+  }
+}
+
+/**
+ * Function replaces main with results view
+ */
+function displayScore() {
+  console.log('test');
+}
+
+
+
 function listener() {
   loadApp();
-  startQuiz();
+  playQuiz();
   submitAnswer();
+  progressQuiz();
+  displayScore();
 }
 
 $(listener);
