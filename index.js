a43fab20362ebb3d86d03612dd4cac7485419f8a
Leon Dail <leondailofficial@gmail.com>
2019-08-16 13:12:14 -0700

    working on submitAnswer

--- index.js
+++ index.js
@@ -11,38 +11,52 @@ function loadApp() {
     <form id="startQuiz">
       <button type="submit">Start Quiz</button>
     </form>
-
   `);
 }
 
 function startQuiz() {
-  console.log('start quiz begin');
   $('#startQuiz').submit( e => {
+    let score =0;
+    let currentQuestionNumber = 1;
     e.preventDefault();
-    console.log('function catch');
-    let score;
-    let currentQuestionNumber;
-
     $('main').empty();
     $('main').append(`
     <h1>Game of Thrones Quiz</h1>
     <ul>
       <li>Score: ${score}</li>
-      <li>Question: ${currentQuestionNumber} out of ${database.length}{</li>
+      <li>Question: ${currentQuestionNumber} out of ${database.length} </li>
     </ul>
+    <form id="quizQuestions">
+      <h2>${database[currentQuestionNumber]['question']}</h2>
+      <label for="key1">${database[currentQuestionNumber]['answer'][0]}</label>
+      <input type="radio" name="key" id="key1" required="required" />
+      <label for="key2">${database[currentQuestionNumber]['answer'][1]}</label>
+      <input type="radio" name="key" id="key2" required="required" />
+      <label for="key3">${database[currentQuestionNumber]['answer'][2]}</label>
+      <input type="radio" name="key" id ="key3" required="required" />
+      <label for="key4">${database[currentQuestionNumber]['answer'][3]}</label>
+      <input type="radio" name="key" id ="key4" required="required" />
+      <button type="submit">Submit Answer</button>
+    </form>
     `);
+  });
+}
+
+// $('input:checked').val()
+
+function submitAnswer() {
+  $('main').on('submit','#quizQuestions', e => {
+    let choice = $('#quizQuestions.input:checked').val();
+    console.log(choice);
+    e.preventDefault();
+    $('#quizQuestions').remove();
   });
 }
 
 function listener() {
   loadApp();
   startQuiz();
-  // $('#startQuiz').on('submit', 'button', e => 
-  // {
-  //   e.preventDefault();
-  //   console.log('submit success');
-  // }
-  // );
+  submitAnswer();
 }
 
 $(listener);
@@ -53,7 +67,7 @@ $(listener);
  * ***Event Listeners***
  * loadApp - generate quiz page / also restart quiz page
  * startQuiz - generates question page/form after clicking 'begin' button
- * submitAnswer - listens when user submits an answer choice
+ * submitAnswer - listens when user submits an answer choice and provides correct/incorrect
  * nextQuestion - listens for when a user pressed 'next question'
  *
  * 
