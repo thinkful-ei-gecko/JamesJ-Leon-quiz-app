d99bd864c92c2efa554f5dcd8e3a78d941cdfa6e
Leon Dail <leondailofficial@gmail.com>
2019-08-16 11:41:27 -0700

    added initial quiz page

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Function loads the quiz page
+ */
+function loadApp() {
+  $('section').empty();
+  $('section').append(`
+    <h1>Game of Thrones Quiz</h1>
+    <p>Behold - this is the toughest quiz about Games of Thrones ever devised. Enjoy!</p>
+    <form id="startQuiz">
+      <button type="submit">Start Quiz</button>
+    </form>
+
+  `);
+}
+
+function listener() {
+  loadApp();
+}
+
+$(listener);
+
+/**
+ * Functions that we know we will need:
+ * 
+ * ***Event Listeners***
+ * loadApp - generate quiz page / also restart quiz page
+ * startQuiz - generates question page/form after clicking 'begin' button
+ * submitAnswer - listens when user submits an answer choice
+ * nextQuestion - listens for when a user pressed 'next question'
+ *
+ * 
+ */
